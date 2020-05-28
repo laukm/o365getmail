0596abda16aa0331ed86c3f83c4a100b8ca5a0e1
Manuel Lauk <manuel.lauk@zeiss.com>
2020-05-28 09:15:18 +0200

    Initial

---
+++ o365getmail/config.py
@@ -0,0 +1,54 @@
+"""
+    Configuration settings for running the o365getmail script.
+
+    In productive system, ensure file can't be accessed by unpriviledged.
+"""
+
+import os
+
+CWD = os.path.dirname(os.path.abspath(__file__))
+
+
+CLIENT_ID = 'e0b4909c-1802-4431-874f-824b5590827f'
+CLIENT_SECRET = 'zHix7WsUU7/g?.]9drRLfFj36Pt:E897'
+
+
+MAIL_PATH = os.path.join(CWD, 'mails')
+TOKEN_PATH = os.path.join(CWD, 'tokens')
+LOG_PATH = os.path.join(CWD, 'o365_email_getter.log')
+
+
+
+# AUTHORITY_URL ending determines type of account that can be authenticated:
+# /organizations = organizational accounts only
+# /consumers = MSAs only (Microsoft Accounts - Live.com, Hotmail.com, etc.)
+# /common = allow both types of accounts
+AUTHORITY_URL = 'https://login.microsoftonline.com/common'
+
+AUTH_ENDPOINT = '/oauth2/v2.0/authorize'
+TOKEN_ENDPOINT = '/oauth2/v2.0/token'
+
+RESOURCE = 'https://graph.microsoft.com/'
+API_VERSION = 'beta'
+#['basic', 'message_all']
+SCOPES = ['User.Read', 'offline_access', 'Mail.ReadWrite', 'Mail.Send'] # Add other scopes/permissions as needed.
+
+
+# Getter definitions for message pull
+USERS = []
+USERS.append({"user_id":"dev9.requesttracker@zeiss.com", "queue":"Microsurgery", "action":"correspond"})
+#USERS.append({"user_id":"dev8.requesttracker@zeiss.com", "queue":"Ophthalmic", "action":"correspond"})
+
+
+# MDA settings
+RT_URL = 'https://dev-med-rt.zeiss.com/'
+CA_FILE = '/usr/local/share/ca-certificates/dev-med-rt.zeiss.com/dev_med_rt.zeiss.com.cer'
+# "MDA": "/opt/rt4/bin/rt-mailgate --queue 'Microsurgery' --action correspond --url https://dev-med-rt.zeiss.com/ --ca-file /usr/local/share/ca-certificates/dev-med-rt.zeiss.com/dev_med_rt.zeiss.com.cer"
+
+
+
+# This code can be removed after configuring CLIENT_ID and CLIENT_SECRET above.
+if 'ENTER_YOUR' in CLIENT_ID or 'ENTER_YOUR' in CLIENT_SECRET or 'ENTER_YOUR' in MAIL_PATH or 'ENTER_YOUR' in TOKEN_PATH or 'ENTER_YOUR' in LOG_PATH:
+    print('ERROR: config.py does not contain valid CLIENT_ID and CLIENT_SECRET')
+    import sys
+    sys.exit(1)
